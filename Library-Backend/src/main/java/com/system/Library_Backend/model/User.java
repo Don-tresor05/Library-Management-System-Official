b985911c26c1691b6a4b8e060e7becc4346247b5
Don-tresor05 <dontresor922@gmail.com>
2025-04-11 02:55:20 +0530

    now User and Book tables ready

---
+++ Library-Backend/src/main/java/com/system/Library_Backend/model/User.java
@@ -0,0 +1,33 @@
+package com.system.Library_Backend.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
