e15932d9ad928c6a0e7cab1afe22d9abe204efcb
Don-tresor05 <dontresor922@gmail.com>
2025-04-27 11:17:38 +0530

    Small Change

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -1,32 +1,3 @@
-// // src/App.jsx
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import BookList from './pages/BookList';
-// import UserManagement from './pages/UserManagement';
-// import LoanManagement from './pages/LoanManagement';
-// import './styles/main.css';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="app">
-//         <Navbar />
-//         <main className="container">
-//           <Routes>
-//             <Route path="/" element={<BookList />} />
-//             <Route path="/users" element={<UserManagement />} />
-//             <Route path="/loans" element={<LoanManagement />} />
-//           </Routes>
-//         </main>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
